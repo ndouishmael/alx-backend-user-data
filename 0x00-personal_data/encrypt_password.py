251c21008f175e034c409ff7f110396733a56391
Rotondwa Ishmael Ndou <111059267+ndouishmael@users.noreply.github.com>
2023-08-03 21:20:09 +0200

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-
+""" Returns a salted, hashed password, byte in string """
 import bcrypt
 
-def hash_password(password: str) -> bytes:
-    """Hashes the password using bcrypt."""
-    # Generate a random salt and hash the password
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
 
-if __name__ == '__main__':
-    # Example usage
-    password = "my_secure_password"
-    hashed_password = hash_password(password)
-    print(hashed_password)
+def hash_password(password: str) -> bytes:
+    """ Returns byte string password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Implement is_valid to validate provided password
+    matched hashed_password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
